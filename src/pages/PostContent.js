80fc23b98e43205163f584c69474e71dd0d2103e
Sofia Rodriguez <sofiaromorales@gmail.com>
2022-01-12 23:35:54 -0400

    react markdown styling

--- src/pages/PostContent.js
+++ src/pages/PostContent.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import ReactMarkdown from 'react-markdown'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import moment from 'moment'
 
 import { API_ROOT } from '../constants'
 
@@ -14,18 +15,19 @@ const POST_LINK = '/posts/'
 class PostContent extends React.Component {
 
     state = {
-        post: null
+        date: null,
+        post: null,
+        time: null,
+        title: null
     }
 
     componentDidMount(){
-        console.log('this.props.match');
-        console.log(this.props);
         if (this.props.match.params.id != null){
             fetch(`${API_ROOT}/post/${this.props.match.params.id}`)
                 .then(response => {
                     if (response.ok){
                         return Promise.resolve(response)
-                    }else{
+                    } else {
                         throw new Error()
                     }
                 })
@@ -33,11 +35,16 @@ class PostContent extends React.Component {
                 .then(response => {
                     const post = response
                     this.setState({ post: post })
-                    //this.setState({ title: this.props.match.params.id.slice(0, this.props.match.params.id.indexOf('_',1))})
-                    //this.setState({date: this.props.match.params.id.slice(this.props.match.params.id.indexOf('_',1) + 1, -3)})
-                    //this.setState({time: post.slice(0, post.indexOf('\n',1))})
+                    this.setState({ title: this.props.match.params.id.slice(0, this.props.match.params.id.indexOf('_',1)) })
+                    this.setState({
+                        date: this.props.match.params.id.slice(
+                                this.props.match.params.id.indexOf('_',1) + 1,
+                                this.props.match.params.id.indexOf('_', this.props.match.params.id.indexOf('_',1) + 2)
+                            )
+                    })
+                    this.setState({ time: post.slice(0, post.indexOf('\n',1)) })
                 })
-                .catch(error =>{
+                .catch(error => {
                     console.log('An error ocurred:');
                     console.log(error);
                 })
@@ -62,8 +69,14 @@ class PostContent extends React.Component {
                                 </div>
                             );
                         }
-                        // Return default child if it's not an image
-                        return <p>{children}</p>;
+                        return (
+                            <div className='my-2'>
+                                <p className='text-md leading-loose'>
+                                    {children}
+                                </p>
+                            </div>
+                        )
+
                     },
                     code({ node, inline, className, children, ...props }) {
                         const match = /language-(\w+)/.exec(className || '')
@@ -72,7 +85,7 @@ class PostContent extends React.Component {
                                 children={String(children).replace(/\n$/, '')}
                                 style={dracula}
                                 language={match[1]}
-                                PreTag="div"
+                                PreTag='div'
                                 {...props}
                             />
                         ) : (
@@ -88,8 +101,6 @@ class PostContent extends React.Component {
 
     render() {
 
-
-
         return (
             <div className='PostContent grid grid-rows-max'>
                 <div className='top-bar py-8 px-36'>
@@ -98,7 +109,7 @@ class PostContent extends React.Component {
                     </p>
                 </div>
                 <hr/>
-                <div className='px-96 pt-10 mb-20'>
+                <div className='px-96 pt-10'>
                     <Link to={POST_LINK}>
                         <p className='secondary-text text-lg'>
                             {BACK_TO_POSTS}
@@ -106,6 +117,12 @@ class PostContent extends React.Component {
                     </Link>
                 </div>
                 <div className='px-96 pt-10 mb-20'>
+                    <p className='mb-10'>
+                        {`${moment(this.state.date).format('MMMM Do YYYY')}  🔥 ${this.state.time}  🔥`}
+                    </p>
+                    <p className='text-5xl'>
+                        {this.state.title}
+                    </p>
                     {this.state.post &&
                         this.renderPost(this.state.post)
                     }
@@ -113,7 +130,7 @@ class PostContent extends React.Component {
                 <div className='px-96'>
                     <PostFooter/>
                 </div>
-                <div className='grid grid-flow-col auto-cols-max px-96 mt-20 justify-center'>
+                <div className='grid grid-flow-col auto-cols-max px-96 mt-20 justify-center mb-20'>
                     <Link to={POST_LINK}>
                         <div className='back-to-posts px-5 py-2 border rounded border-4'>
                             <p className='secondary-text font-extrabold text-center text-lg'>
