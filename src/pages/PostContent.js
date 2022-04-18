fc2d32e4e5bd9abed14b7eaf15e04a4205d7055c
Sofía Rodríguez <sofiaromorales@gmail.com>
2022-04-17 23:12:59 -0400

    Update PostContent.js

--- src/pages/PostContent.js
+++ src/pages/PostContent.js
@@ -81,7 +81,6 @@ class PostContent extends React.Component {
                                 </p>
                             </div>
                         )
-
                     },
                     code({ node, inline, className, children, ...props }) {
                         const match = /language-(\w+)/.exec(className || '')
